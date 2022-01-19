3a861f357f11cceb8f6b1a91c23a368025d9c4a1
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2022-01-19 17:00:20 +0000

    complete exercise 6 and 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please provide a store's name"
+storeName = gets.chomp
+
+@createStore = Store.create(name: storeName)
+
+puts @createStore.errors.full_messages
+
+# same thing
+# @createStore.errors.full_messages.each do |err_msg|
+#     puts err_msg
+# end
+
+@createStore2 = Store.create(name: 'Victoria', annual_revenue: 300000, mens_apparel: false, womens_apparel: false)
+puts @createStore2.errors.full_messages
